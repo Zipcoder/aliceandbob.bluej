d7dc0a5cdf2b91b51a291aac7b78988ac17d3d0f
zipcodeFrancisco <zipcodefrancisco@gmail.com>
2019-02-05 18:00:35 -0500

    Completed and Tested =)

--- Main.java
+++ Main.java
@@ -1,9 +1,24 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args ){
-
+        System.out.print('\u000C');
+        String name;
+        System.out.println("What's your name?" );
+        Scanner input = new Scanner(System.in);
+        do{
+            name = input.next();
+            if( name.equals("Bob") || name.equals("Alice")){
+                System.out.print("\nHello " + name + "!");
+                break;
+            }
+            else{
+                System.out.print("You are neither a Bob or an Alice, try again!\n");
+            }
+        } while( true );
     }
+    
 }
