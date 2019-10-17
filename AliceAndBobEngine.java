9954a2a8494999af1beb24a7e9e97d673ff9836b
Eddie Morales <eddie@zipcoders-MacBook-Pro.local>
2019-10-16 20:10:57 -0400

    solved test cases; added additional 4 tests; removed 1 test

--- AliceAndBobEngine.java
+++ AliceAndBobEngine.java
@@ -1,22 +1,12 @@
-
-/**
- * Write a description of class AliceAndBobEngine here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
-public class AliceAndBobEngine
-{
-    String result;
-    
-    AliceAndBobEngine(){
-        result = "";
-    }
-
+public class AliceAndBobEngine {
     //HINT: LOOK AT THE TESTS TO SEE WHAT IS EXPECTED FOR DIFFERENT CONDITIONS
     public String evaluateAliceOrBob(String input)
     {
-    
-        return result;
+        if( input == "Alice" || input == "Bob" ){
+            return "Hello " + input;
+
+        }else{
+            return "I DON'T KNOW YOU, YOU ARE SCARY";
+        }
     }
 }
