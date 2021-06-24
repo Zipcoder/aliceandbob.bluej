0d9246ad0cf679c0c9f5cf350dc230545f2b763f
johnS93 <john.song8293@gmail.com>
2021-06-24 19:38:37 -0400

    I did the lab changes

--- AliceAndBobEngine.java
+++ AliceAndBobEngine.java
@@ -9,17 +9,21 @@ public class AliceAndBobEngine {
      * @param input - this value is variable: it has the potential to be many things
      * @return `true` if `input` is "Alice"
      */
-    public Boolean isAlice(String input) {
-        return null;
-    }
 
+    public Boolean isAlice(String input) {
+        return input.equals("Alice");
+        
+    }
+    
+
+    
     /**
      * return `true` if the input value is "Bob"
      * @param input - this value is variable: it has the potential to be many things
      * @return `true` if `input` is "Bob"
      */
     public Boolean isBob(String input) {
-        return null;
+        return input.equals("Bob");
     }
 
     /**
@@ -28,8 +32,17 @@ public class AliceAndBobEngine {
      * @return `true` if `input` is "Alice" or "Bob"
      */
     public Boolean isAliceOrBob(String input) {
-        return null;
-    }
+        if(input == "Alice"){
+            return true;
+        };
+         
+        if(input == "Bob") {
+           
+            return true;
+        }
+        return false;
+    } 
+    
 
     /**
      * if the input value is "Alice" or "Bob", then
@@ -42,6 +55,12 @@ public class AliceAndBobEngine {
      * @return respective String value
      */
     public String getGreeting(String input) {
-        return null;
+        if((input == "Alice") || (input == "Bob")) {
+            return "Hello, "+input+"!";
+        }
+        else {
+            return "Begone, "+input+"! You're a stranger!";
+        }
     }
 }
+
