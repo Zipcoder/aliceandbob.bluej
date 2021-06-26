e81b2cbfbde47ec7bb7667083cc49cd0dbf07630
Raymond <raymond@zipcoder.local>
2021-06-25 22:23:11 -0400

    Raymond Fitzgerald

--- AliceAndBobEngine.java
+++ AliceAndBobEngine.java
@@ -10,8 +10,11 @@ public class AliceAndBobEngine {
      * @return `true` if `input` is "Alice"
      */
     public Boolean isAlice(String input) {
-        return null;
-    }
+        if (input == "Alice"){
+       return true;
+   }else return false;
+    
+}
 
     /**
      * return `true` if the input value is "Bob"
@@ -19,8 +22,11 @@ public class AliceAndBobEngine {
      * @return `true` if `input` is "Bob"
      */
     public Boolean isBob(String input) {
-        return null;
-    }
+        if (input == "Bob"){
+        return true;
+    }else return false;
+    
+}
 
     /**
      * return `true` if the input value is "Alice" or "Bob"
@@ -28,8 +34,11 @@ public class AliceAndBobEngine {
      * @return `true` if `input` is "Alice" or "Bob"
      */
     public Boolean isAliceOrBob(String input) {
-        return null;
-    }
+        if (input == "Alice"||input == "Bob"){
+            return true;
+    }else return false;
+    
+}
 
     /**
      * if the input value is "Alice" or "Bob", then
@@ -42,6 +51,13 @@ public class AliceAndBobEngine {
      * @return respective String value
      */
     public String getGreeting(String input) {
-        return null;
+        if (input == "Alice"){
+        return "Hello Alice";
+    }  else if (input == "Bob"){
+        return "Hello Bob";
+    }  else {
+        return "Begone, input!You're a stranger!";
     }
 }
+};
+
